18876c093fe7a5137b41ac9b1d979a5a0d0f6cb3
Milton-Luis <miltonluis138@gmail.com>
2025-06-11 21:41:19 -0300

    Refactor: Rename PDFReader to PDFFileReader and adjust formatting

--- app/utils/pdf/reader.py
+++ app/utils/pdf/reader.py
@@ -1,10 +1,11 @@
 from typing import List
-from astral_map.app.utils.pdf.loader import PDFLoader
+from app.utils.pdf.loader import PDFLoader
 
 
-class PDFReader:
+class PDFFileReader:
     def __init__(self, pdf_loader: PDFLoader):
         self._pdf_loader = pdf_loader
+
     def extract_text_by_title(self, filename: str, title: str, title_list: List[str]):
         full_text = self._pdf_loader.load_pdf_text(filename)
 
@@ -16,7 +17,8 @@ class PDFReader:
             return f"Título '{title}' não encontrado no texto."
 
         endless_index = len(full_text)
-        remaining_titles = title_list[title_list.index(title.upper()) + 1:]
+        remaining_titles = title_list[title_list.index(title.upper()) + 1 :]
+        
         for next in remaining_titles:
             next_title = full_text.find(next, initial_index + 1)
             if next_title != -1:

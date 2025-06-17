f5b51da27040ceaa5a14e964670c2098ce977a8e
Milton-Luis <miltonluis138@gmail.com>
2025-06-16 22:15:24 -0300

    Refactor: Introduce SingleAstralForm for better structure and remove unused fields

--- app/forms/astral_vedic_form.py
+++ app/forms/astral_vedic_form.py
@@ -1,8 +1,33 @@
 from flask_wtf import FlaskForm
-from wtforms import ValidationError, widgets
-from wtforms.fields import (SelectField, SelectMultipleField, StringField,
-                            SubmitField)
+from wtforms import FieldList, FormField, ValidationError, widgets
+from wtforms.fields import SelectField, SelectMultipleField, StringField, SubmitField
 from wtforms.validators import DataRequired
+
+
+class SingleAstralForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    house = StringField("Casa", validators=[DataRequired()])
+    planet = SelectMultipleField(
+        "Planeta",
+        choices=[
+            ("Sol", "Sol"),
+            ("Lua", "Lua"),
+            ("Marte", "Marte"),
+            ("Mercúrio", "Mercúrio"),
+            ("Jupíter", "Jupíter"),
+            ("Vênus", "Vênus"),
+            ("Saturno", "Saturno"),
+            ("Nenhum", "Nenhum"),
+        ],
+        option_widget=widgets.CheckboxInput(),
+        widget=widgets.ListWidget(prefix_label=False),
+    )
+    house_strengths = StringField("Força da Casa", validators=[DataRequired()])
+    planet_strengths = StringField("Força do Planeta", validators=[DataRequired()])
+    ishtaphala = StringField("Ishtaphala", validators=[DataRequired()])
+    kashtaphala = StringField("Kashtaphala", validators=[DataRequired()])
 
 
 class AstralForm(FlaskForm):
@@ -39,46 +64,5 @@ class AstralForm(FlaskForm):
             ("VISHAKHA", "VISHAKHA"),
         ],
     )
-    house = SelectField(
-        "Casa",
-        validators=[DataRequired()],
-        choices=[
-            (1, "1ª Casa: A Casa do Eu (Tanu Bhava)"),
-            (2, "2ª Casa: Riqueza e Família (Dhana Bhava)"),
-            (3, "3ª Casa: Comunicação e Coragem (Sahaja Bhava)"),
-            (4, "4ª Casa: Lar e Felicidade (Sukha Bhava)"),
-            (5, "5ª Casa: Criatividade e Filhos (Putra Bhava)"),
-            (6, "6ª Casa: Trabalho e Saúde (Shatru Bhava)"),
-            (7, "7ª Casa: Parcerias e Casamento (Kalatra Bhava)"),
-            (8, "8ª Casa: Transformação e Segredos (Ayur Bhava)"),
-            (9, "9ª Casa: Sabedoria e Espiritualidade (Dharma Bhava)"),
-            (10, "10ª Casa: Carreira e Reputação (Karma Bhava)"),
-            (11, "11ª Casa: Ganhos e Desejos (Labha Bhava)"),
-            (12, "12ª Casa: Perdas e Liberação (Vyaya Bhava)"),
-        ],
-    )
-    planet = SelectMultipleField(
-        "Planeta",
-        choices=[
-            ("Sol", "Sol"),
-            ("Lua", "Lua"),
-            ("Marte", "Marte"),
-            ("Mercúrio", "Mercúrio"),
-            ("Jupíter", "Jupíter"),
-            ("Vênus", "Vênus"),
-            ("Saturno", "Saturno"),
-            ("Nenhum", "Nenhum"),
-        ],
-        option_widget=widgets.CheckboxInput(),
-        widget=widgets.ListWidget(prefix_label=False),
-    )
-    house_strengths = StringField("Força da Casa", validators=[DataRequired()])
-    planet_strengths = StringField("Força do Planeta", validators=[DataRequired()])
-    ishtaphala = StringField("Ishtaphala", validators=[DataRequired()])
-    kashtaphala = StringField("Kashtaphala", validators=[DataRequired()])
-
+    houses = FieldList(FormField(SingleAstralForm), min_entries=2, max_entries=12)
     submit = SubmitField("Enviar")
-
-    def validate_planet(self, field):
-        if not field.data or len(field.data) == 0:
-            raise ValidationError("Selecione pelo menos um planeta.")

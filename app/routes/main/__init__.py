e0a19b93003df3665e765e092c7432a4c0e65e0e
Milton-Luis <miltonluis138@gmail.com>
2025-06-16 22:16:00 -0300

    Fix: Correct duplicate URL rule for settings_astral_vedic_values in main routes

--- app/routes/main/__init__.py
+++ app/routes/main/__init__.py
@@ -6,4 +6,11 @@ main = Blueprint("main", __name__)
 
 
 main.add_url_rule("/", view_func=views.index, endpoint="index", methods=["POST", "GET"])
+
+main.add_url_rule(
+    "/",
+    view_func=views.settings_astral_vedic_values,
+    endpoint="settings_astral_vedic_values",
+    methods=["POST", "GET"],
+)
 main.add_url_rule("/dowload", view_func=views.download_pdf, endpoint="download")
